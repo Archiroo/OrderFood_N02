d5849b8e4813b8fbae5f17fa59ee94dcb56b8a4c
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-04-05 17:59:19 +0700

    Xong giao diện

--- Source_Code/OrderFood/app/src/main/java/com/example/orderfood/MainActivity.java
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.orderfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.orderfood.Processer.DashboardActivity;
+import com.example.orderfood.Processer.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_continue = findViewById(R.id.btn_continue);
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dashboard = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(dashboard);
+            }
+        });
     }
+
 }
