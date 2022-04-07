900dac291db19a56999786c5d3c75237ca9bd4c4
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-04-07 16:31:11 +0700

    Xong bottom navigation

---
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/NotificationFragment.java
@@ -0,0 +1,22 @@
+package com.example.orderfood.Activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.orderfood.R;
+
+
+public class NotificationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_notification, container, false);
+
+        return view;
+    }
+}
