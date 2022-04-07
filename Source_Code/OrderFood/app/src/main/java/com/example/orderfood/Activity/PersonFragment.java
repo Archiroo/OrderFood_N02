3d7d26e41ae13a4116da753371fcc975a270fb8f
PhamDuy <72310645+Phamduy5801@users.noreply.github.com>
2022-04-07 22:08:02 +0700

    Log out

--- Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/PersonFragment.java
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/PersonFragment.java
@@ -1,5 +1,8 @@
 package com.example.orderfood.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +10,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.orderfood.MainActivity;
 import com.example.orderfood.R;
 
 
 public class PersonFragment extends Fragment {
-
+    Button btn_logout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_person, container, false);
+        btn_logout = (Button) view.findViewById(R.id.btn_logout);
 
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog_confirmLogout = new AlertDialog.Builder(getActivity());
+                dialog_confirmLogout.setMessage("Bạn có muốn đăng xuất không ?");
+                dialog_confirmLogout.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Toast.makeText(getActivity(),"Đăng xuất thành công",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                //không xoá
+                dialog_confirmLogout.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                dialog_confirmLogout.show();
+
+            }
+        });
         return view;
     }
+
+
 }
