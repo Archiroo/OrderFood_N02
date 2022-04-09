71f7f3eade902be5076f8784f68b44da717577a0
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-04-10 01:40:51 +0700

    Cũng hòm hòm

--- Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/DetailActivity.java
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/DetailActivity.java
@@ -1,19 +1,34 @@
 package com.example.orderfood.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.orderfood.Adapter.CartRecyclerViewAdapter;
+import com.example.orderfood.Adapter.DetailRecyclerViewAdapter;
+import com.example.orderfood.Adapter.HomeRecyclerView1Adapter;
+import com.example.orderfood.Adapter.HomeRecyclerView3Adapter;
+import com.example.orderfood.Model.HomeRecyclerview2;
 import com.example.orderfood.Model.HomeRecyclerview3;
 import com.example.orderfood.R;
+import com.google.firebase.database.core.Context;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
     private ImageView btn_goBack;
+
+    private RecyclerView rcv1_Detail;
+    private DetailRecyclerViewAdapter rcv_detailAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +60,41 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
+        TextView btn_addCart1 = findViewById(R.id.btn_addCart1);
+        btn_addCart1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Thêm vào giỏ hàng thành công", Toast.LENGTH_SHORT).show();
 
+            }
+        });
+
+        TextView btn_addCart = findViewById(R.id.btn_addCart);
+        btn_addCart1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Thêm vào giỏ hàng thành công", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        
+
+
+        //Recycle view
+        ArrayList<HomeRecyclerview3> mList_detail = new ArrayList<HomeRecyclerview3>();
+        // Sử dụng vòng lặp for và setter
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat1, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat2, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat3, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat4, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat5, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+        mList_detail.add(new HomeRecyclerview3(R.drawable.cat6, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
+
+        rcv1_Detail = findViewById(R.id.detail_rcv1);
+        rcv_detailAdapter = new DetailRecyclerViewAdapter(mList_detail);
+        rcv1_Detail.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rcv1_Detail.setAdapter(rcv_detailAdapter);
 
 
     }
---
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Adapter/DetailRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.orderfood.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.orderfood.Model.HomeRecyclerview3;
+import com.example.orderfood.R;
+
+import java.util.ArrayList;
+
+public class DetailRecyclerViewAdapter extends RecyclerView.Adapter<DetailRecyclerViewAdapter.DetailRecyclerViewHolder>{
+
+    public ArrayList<HomeRecyclerview3> mList_detailItem;
+
+    public DetailRecyclerViewAdapter(ArrayList<HomeRecyclerview3> mList_detailItem) {
+        this.mList_detailItem = mList_detailItem;
+    }
+
+    @NonNull
+    @Override
+    public DetailRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.detail_rcv1_item, parent, false);
+
+        return new DetailRecyclerViewAdapter.DetailRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailRecyclerViewHolder holder, int position) {
+
+        HomeRecyclerview3 currentItem_detail = mList_detailItem.get(position);
+        if(currentItem_detail == null){
+            return;
+        }
+
+        // Khác null thực hiện
+        holder.detail_image.setImageResource(currentItem_detail.getImageFood());
+        holder.detail_name.setText(currentItem_detail.getNameFood());
+        holder.detail_detail.setText(currentItem_detail.getNameFood());
+        holder.detail_price.setText(currentItem_detail.getDetailFood());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mList_detailItem != null){
+            return mList_detailItem.size();
+        }
+        return 0;
+    }
+
+    public class DetailRecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView detail_image;
+        public TextView detail_name;
+        public TextView detail_price;
+        public TextView detail_detail;
+        public TextView detail_btnAdd;
+        public ConstraintLayout cart_layoutItem;
+
+        public DetailRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            detail_image = itemView.findViewById(R.id.detail_itemImage);
+            detail_name = itemView.findViewById(R.id.detail_itemNameFood);
+            detail_price = itemView.findViewById(R.id.detail_money);
+            detail_detail = itemView.findViewById(R.id.detail_itemDetail);
+            detail_btnAdd = itemView.findViewById(R.id.btn_addCart);
+            cart_layoutItem = itemView.findViewById(R.id.detail_layout_rcv);
+
+
+        }
+    }
+}
