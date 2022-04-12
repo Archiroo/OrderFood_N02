51e86c9659e6932c000e328b67927219d88891a0
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-04-12 12:25:19 +0700

    Xong gio hang

--- Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/CartFragment.java
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Activity/CartFragment.java
@@ -1,5 +1,9 @@
 package com.example.orderfood.Activity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,11 +13,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.orderfood.Adapter.CartRecyclerViewAdapter;
 import com.example.orderfood.Adapter.HomeRecyclerView1Adapter;
+import com.example.orderfood.Database.DBhelper;
 import com.example.orderfood.Model.HomeRecyclerview3;
+import com.example.orderfood.Model.ObjectFood;
 import com.example.orderfood.R;
 
 import java.util.ArrayList;
@@ -22,23 +31,51 @@ public class CartFragment extends Fragment {
 
     private RecyclerView rcv_cart;
     private CartRecyclerViewAdapter rcv_cartAdapter;
+    Button btn_confirmOrder;
+
+    SQLiteDatabase sqLitedb = SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.orderfood/databases/OrderFoodN02.sqlite", null);
+    Cursor cursor = null, cursor1 = null;
+    DBhelper db = new DBhelper(getActivity());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_cart, container, false);
 
+        ObjectFood ob_food = new ObjectFood();
+        ArrayList<ObjectFood> mList_cart = new ArrayList<ObjectFood>();
+        String sql = "SELECT * FROM tb_cart WHERE CART_STATUS = 1";
+        cursor = sqLitedb.rawQuery(sql, null);
+        mList_cart.clear();
+        for(int i = 0; i < cursor.getCount(); i++){
+            cursor.moveToPosition(i);
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String price = cursor.getString(2);
+            int number =  cursor.getInt(3);
+            byte[] image = cursor.getBlob(4);
+            mList_cart.add(new ObjectFood(id, image, name, price, number));
+        }
 
-        ArrayList<HomeRecyclerview3> mList_cart = new ArrayList<HomeRecyclerview3>();
-        // Sử dụng vòng lặp for và setter
-        mList_cart.add(new HomeRecyclerview3(R.drawable.cat1, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
-        mList_cart.add(new HomeRecyclerview3(R.drawable.cat1, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
-        mList_cart.add(new HomeRecyclerview3(R.drawable.cat1, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
-        mList_cart.add(new HomeRecyclerview3(R.drawable.cat1, "Burger Bò Nướng", "115.000 VNĐ", "Rất ngon"));// thiết lập giá trị
-
+        //Hien thi list
         rcv_cart = view.findViewById(R.id.cart_rcv);
-        rcv_cartAdapter = new CartRecyclerViewAdapter(mList_cart);
+        rcv_cartAdapter = new CartRecyclerViewAdapter(mList_cart, getActivity());
         rcv_cart.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         rcv_cart.setAdapter(rcv_cartAdapter);
+        Fragment fragment = null;
+
+        // Su kien
+        btn_confirmOrder = view.findViewById(R.id.btn_confirmOrder);
+        btn_confirmOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put("cart_status", 2);
+                sqLitedb.update("tb_cart", values, "cart_status = 1", null);
+                Intent intent = new Intent(getActivity(),DashboardActivity.class);
+                startActivity(intent);
+                Toast.makeText(getActivity(),"Đặt hàng thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
 
--- Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Adapter/CartRecyclerViewAdapter.java
+++ Source_Code/OrderFood/app/src/main/java/com/example/orderfood/Adapter/CartRecyclerViewAdapter.java
@@ -1,27 +1,42 @@
 package com.example.orderfood.Adapter;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.orderfood.Activity.DashboardActivity;
 import com.example.orderfood.Model.HomeRecyclerview3;
+import com.example.orderfood.Model.ObjectFood;
 import com.example.orderfood.R;
 
 import java.util.ArrayList;
 
 public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerViewAdapter.CartRecyclerViewHolder>{
 
-    public ArrayList<HomeRecyclerview3> cart_item;
+    public ArrayList<ObjectFood> cart_item;
 
-    public CartRecyclerViewAdapter(ArrayList<HomeRecyclerview3> cart_item) {
+    public Context mcontex;
+
+    public CartRecyclerViewAdapter(ArrayList<ObjectFood> cart_item, Context mcontex) {
         this.cart_item = cart_item;
+        this.mcontex = mcontex;
     }
+    SQLiteDatabase sqLitedb = SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.orderfood/databases/OrderFoodN02.sqlite", null);
+    Cursor cursor = null;
 
     @NonNull
     @Override
@@ -34,15 +49,54 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
     @Override
     public void onBindViewHolder(@NonNull CartRecyclerViewHolder holder, int position) {
 
-        HomeRecyclerview3 current_cartItem = cart_item.get(position);
-        if(current_cartItem == null){
+        ObjectFood currentItem = cart_item.get(position);
+        if(currentItem == null){
             return;
         }
 
         // Khác null thực hiện
-        holder.cart_image.setImageResource(current_cartItem.getImageFood());
-        holder.cart_name.setText(current_cartItem.getNameFood());
-        holder.cart_price.setText(current_cartItem.getPriceFood());
+        Bitmap bitmap = BitmapFactory.decodeByteArray(currentItem.getImageFood(), 0, currentItem.getImageFood().length);
+        holder.cart_image.setImageBitmap(bitmap);
+        holder.cart_name.setText(currentItem.getNameFood());
+        holder.cart_price.setText(currentItem.getPriceFood());
+        holder.cart_number.setText(String.valueOf(currentItem.getNumber()));
+        holder.ic_minimus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number1 = Integer.parseInt(holder.cart_number.getText()+"");
+                if(number1>1){
+                    number1--;
+                }
+                holder.cart_number.setText(String.valueOf(number1));
+                ContentValues values = new ContentValues();
+                values.put("cart_number", number1);
+                String sql = "cart_id = "+ currentItem.getId()+"";
+                sqLitedb.update("tb_cart", values, sql, new String[]{});
+            }
+        });
+
+        holder.ic_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number1 = Integer.parseInt(holder.cart_number.getText()+"");
+                number1++;
+                holder.cart_number.setText(String.valueOf(number1));
+                ContentValues values = new ContentValues();
+                values.put("cart_number", number1);
+                String sql = "cart_id = "+ currentItem.getId()+"";
+                sqLitedb.update("tb_cart", values, sql, new String[]{});
+            }
+        });
+        holder.cart_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sql = "cart_id = "+ currentItem.getId()+"";
+                sqLitedb.delete("tb_cart", sql, null);
+                Intent intent = new Intent(mcontex, DashboardActivity.class);
+                mcontex.startActivity(intent);
+                Toast.makeText(mcontex,"Xóa thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -56,9 +110,8 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
 
     public class CartRecyclerViewHolder extends RecyclerView.ViewHolder{
 
-        public ImageView cart_image;
-        public TextView cart_name;
-        public TextView cart_details;
+        public ImageView cart_image,ic_minimus, ic_plus, cart_close;
+        public TextView cart_name, cart_number;
         public TextView cart_price;
         public ConstraintLayout cart_layoutItem;
 
@@ -68,6 +121,10 @@ public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerVi
             cart_name = itemView.findViewById(R.id.cart_nameFood);
             cart_price = itemView.findViewById(R.id.cart_price);
             cart_layoutItem = itemView.findViewById(R.id.cart_layoutItem);
+            ic_minimus = itemView.findViewById(R.id.ic_minimus);
+            ic_plus = itemView.findViewById(R.id.ic_plus);
+            cart_number = itemView.findViewById(R.id.cart_number);
+            cart_close = itemView.findViewById(R.id.cart_close);
         }
     }
 
